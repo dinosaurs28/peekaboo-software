f6db415f7c7464712a9ca33310a932548c9f38c8
painarasimha <pai.narasimha04@gmail.com>
2025-11-05 22:46:23 +0530

    [feat] remove legacy print routes in favor of unified receipt page

--- app/invoices/[id]/print-a4/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { notFound } from "next/navigation";
-
-export default function LegacyPrintA4Removed() {
-  // Route removed in favor of unified /invoices/receipt/[id]
-  notFound();
-  return null;
-}
