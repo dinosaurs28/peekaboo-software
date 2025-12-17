2a2a059eb3649d01d02d4074630581c20cfdda9f
prakashwaddar628 <prakashwaddar628@gmail.com>
2025-12-17 10:44:58 +0530

    input placeholder fix

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -9,9 +9,9 @@ const Input = React.memo(React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background",
+          "flex h-10 w-full rounded-md border bg-background px-3 py-2 text-sm ring-offset-background",
           "file:border-0 file:bg-transparent file:text-sm file:font-medium",
-          "placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus-visible:ring-2",
+          "placeholder:text-muted-foreground focus:outline-none focus:ring-1",
           "disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
