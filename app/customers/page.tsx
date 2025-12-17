030fcd119e34bcd4146b8906f6e89a5bd4d7a5ff
prakashwaddar628 <prakashwaddar628@gmail.com>
2025-12-17 14:06:52 +0530

    improve the code quality

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -82,11 +82,11 @@ export default function CustomersPage() {
   // if (!user) return null;
 
   return (
-    <div className="flex min-h-screen w-full bg-gray-50 text-foreground">
+    <div className="flex w-full h-screen bg-slate-50">
       <div className="flex h-[100%]">
         <Sidebar />
       </div>
-      <div className="flex flex-col flex-1">
+      <div className="flex flex-1 flex-col overflow-hidden md:ml-1">
         <Topbar />
         <main className="flex-1 p-8 space-y-6">
           <h1 className="text-3xl font-bold text-gray-900">Customers</h1>
